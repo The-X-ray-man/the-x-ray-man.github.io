1390157db5918c6c73da047d2561e0cf05445a22
The-X-ray-man <petouhoff@gmail.com>
2026-04-22 19:49:10 +0200

    asd

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ function solve(){
             throw new Error(`Response status: ${response.status}`);
         }
         const result = await response.json();
+        console.log(result);
         return(result);
         }     
     catch (error) {
